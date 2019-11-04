43580da208bc8121b32a7fbaf8475883e8f794c0
vpilly <varun.pilly@gmail.com>
2019-11-04 15:38:22 -0500

    added some database commands and test file

--- test.js
+++ test.js
@@ -3,14 +3,35 @@ const connection = require("./config/mongoConnection");
 
 
 async function main() {
-    const user = await users.addUser("Varun", "Pilly", "pass");
-    console.log(user)
+    const user1 = await users.addUser("Varun", "Pilly", "pass");
+    console.log(user1)
+    console.log();
 
-    const db = await connection();
-    await db.serverConfig.close();
+    const user2 = await users.addUser("Daniel", "Collins", "pass");
+    console.log(user2)
+    console.log();
+
+    await users.addUser("Shaopeng", "Ge", "pass");
+
+    console.log(await users.getAllUsers());
+    console.log();
+
+    console.log(await users.removeUser(String(user1._id)));
+
+    console.log(await users.getAllUsers());
+    console.log();
+
+    console.log(await users.renameUser(String(user2._id), "Jonathan", "Alcantara"));
+    console.log();
+
+    console.log(await users.getAllUsers());
 }
 
+// Not quite sure if this is the correct way to do things
 main().catch(function (error) {
     console.error(error);
+}).then(async function () {
+    const db = await connection();
+    await db.serverConfig.close();
 });
 
