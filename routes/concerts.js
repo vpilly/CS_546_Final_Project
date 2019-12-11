11b915cebbcde707b070cc189609a3e3bf19acbb
ShaopengGe <sge2@stevens.edu>
2019-12-10 21:36:57 -0500

    views

--- routes/concerts.js
+++ routes/concerts.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const concertData = require('../data/concerts');
 
-router.get('/', async (req, res) => {
-    res.render("layouts/concertSearch");
+router.post("/", async(req, res) => {
+	try {
+		let peopleFound = await search.getPersonByName(req.body.personName);
+		res.render("posts/search", { title: "People Found", peopleList: peopleFound, personName: req.body.personName });
+	} catch (e) {
+		res.status(400).render('posts/searchError', { title: "400 Error" , error: e });
+		return;
+	}
 });
