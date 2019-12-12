d243ef6e5cc32facc9a70538e9ca58c0a24afebc
ShaopengGe <sge2@stevens.edu>
2019-12-11 21:40:35 -0500

    edit handlebars and route of concerts

--- routes/concerts.js
+++ routes/concerts.js
@@ -4,8 +4,15 @@ const concertData = require('../data/concerts');
 
 router.post("/", async(req, res) => {
 	try {
-		let peopleFound = await search.getPersonByName(req.body.personName);
-		res.render("posts/search", { title: "People Found", peopleList: peopleFound, personName: req.body.personName });
+		let filter = req.body.searchFilter;
+		let concertsFound = [];
+		if(filter == title) concertsFound = await concertData.getConcertByTitle(req.body.searchContent);
+		if(filter == artist) concertsFound = await concertData.getConcertByArtistName(req.body.searchContent);
+		if(filter == address) concertsFound = await concertData.getConcertByAddress(req.body.searchContent);
+		if(filter == genre) concertsFound = await concertData.getConcertByGenre(req.body.searchContent);
+		if(filter == venue) concertsFound = await concertData.getConcertByVenue(req.body.searchContent);
+	
+		res.render("concerts/concertSearchResults", { title: "Concerts Found", concertsList: concertsFound, searchContent: req.body.searchContent });
 	} catch (e) {
 		res.status(400).render('posts/searchError', { title: "400 Error" , error: e });
 		return;
