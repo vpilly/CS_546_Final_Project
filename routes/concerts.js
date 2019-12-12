f8b17e849da7ef0cad495b4047776e806aabb871
ShaopengGe <sxdtgsp@gmail.com>
2019-12-11 23:51:47 -0500

    midify

--- routes/concerts.js
+++ routes/concerts.js
@@ -19,15 +19,16 @@ router.get("/", async(req, res) => {
 
 router.post("/", async(req, res) => {
 	try {
-		let filter = req.body.searchFilter;
+		let body = req.body;
+		let filter = body.searchFilter;
 		let concertsFound = [];
-		if(filter == "title") concertsFound = await concertData.getConcertByTitle(req.body.searchContent);
-		if(filter == "artist") concertsFound = await concertData.getConcertByArtistName(req.body.searchContent);
-		if(filter == "address") concertsFound = await concertData.getConcertByAddress(req.body.searchContent);
-		if(filter == "genre") concertsFound = await concertData.getConcertByGenre(req.body.searchContent);
-		if(filter == "venue") concertsFound = await concertData.getConcertByVenue(req.body.searchContent);
+		if(filter == "title") concertsFound = await concertData.getConcertByTitle(body.searchContent,body);
+		if(filter == "artist") concertsFound = await concertData.getConcertByArtistName(body.searchContent,body);
+		if(filter == "address") concertsFound = await concertData.getConcertByAddress(body.searchContent,body);
+		if(filter == "genre") concertsFound = await concertData.getConcertByGenre(body.searchContent,body);
+		if(filter == "venue") concertsFound = await concertData.getConcertByVenue(body.searchContent,body);
 	
-		res.render("concerts/concertSearchResults", { title: "Concerts Found", concertsList: concertsFound, searchContent: req.body.searchContent });
+		res.render("concerts/concertSearchResults", { title: "Concerts Found", concertsList: concertsFound, searchContent: body.searchContent });
 	} catch (e) {
 		res.status(400).render('concerts/error', { title: "400 Error" , error: e });
 		return;
