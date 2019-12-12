884bf73987e6b00abf7290729706ed9e3945a5e6
ShaopengGe <sge2@stevens.edu>
2019-12-11 21:50:22 -0500

    add more handlebars

--- routes/concerts.js
+++ routes/concerts.js
@@ -14,7 +14,7 @@ router.post("/", async(req, res) => {
 	
 		res.render("concerts/concertSearchResults", { title: "Concerts Found", concertsList: concertsFound, searchContent: req.body.searchContent });
 	} catch (e) {
-		res.status(400).render('posts/searchError', { title: "400 Error" , error: e });
+		res.status(400).render('concerts/concertSearchResultsError', { title: "400 Error" , error: e });
 		return;
 	}
 });
