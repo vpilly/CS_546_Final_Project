560274bf8d1574a9e29cb8062d45cf18b410a17c
JonnyGreat456 <jalcantara598@gmail.com>
2019-12-13 19:35:00 -0500

    added website to artists, fixed login route problem

--- data/artists.js
+++ data/artists.js
@@ -4,14 +4,16 @@ const users = mongoCollections.users;
 const userData = require('./users');
 const { ObjectId } = require('mongodb');
 
-async function addArtist(name, genre) {
-    if (arguments.length !== 2) throw `Error: function artists.addArtist() expected 2 parameters but instead received ${arguments.length}`;
+async function addArtist(name, genre, website) {
+    if (arguments.length !== 3) throw `Error: function artists.addArtist() expected 3 parameters but instead received ${arguments.length}`;
     if (typeof (name) !== 'string') throw `Error: function artists.addArtist() expected name to be a string but instead recieved a ${typeof (name)}`;
     if (typeof (genre) !== 'string') throw `Error: function artists.addArtist() expected genre to be a string but instead recieved a ${typeof (genre)}`;
+    if (typeof (website) !== 'string') throw `Error: function artists.addArtist() expected website to be a string but instead recieved a ${typeof (website)}`;
 
     details = {
         name: name,
         genre: genre,
+        website: website,
         concertsPerformed: [],
         concertsToPerform: []
     }
