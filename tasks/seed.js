aa52a6ac08c31e2076afdc38fbf43ad97ec34ce3
ShaopengGe <sxdtgsp@gmail.com>
2019-12-14 11:35:50 -0500

    seed for concerts

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,7 +1,9 @@
 const dbConnection = require('../config/mongoConnection');
 const artists = require('../data/artists');
-const data = require("./artists_with_site.json");
+const concerts = require('../data/concerts');
 
+const artistsData = require("./artists_with_site.json");
+const concertsData = require("./concerts.json");
 // only artists database seeding implemented
 
 const main = async () => {
@@ -9,10 +11,16 @@ const main = async () => {
     await db.dropDatabase();
 
     let i;
-    for (i = 0; i < data.length; i++) {
-        const row = data[i];
+    for (i = 0; i < artistsData.length; i++) {
+        const row = artistsData[i];
         await artists.addArtist(row.name, row.genre, row.mtv);
     }
+
+    for (index = 0; index < concertsData.length; index++) {
+        const row = concertsData[index];
+        await concerts.addConcert(row.title, row.artists, row.date, row.time, row.address, row.zipcode, row.venue, row.genre, row.description, row.ticketPrice);
+    };
+
 
     await db.serverConfig.close();
 };
