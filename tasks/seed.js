d67d8ebd53465bc185d245c1393dc2b74416da28
ShaopengGe <sxdtgsp@gmail.com>
2019-12-14 14:51:10 -0500

    modify seed.js for concerts seeding

--- tasks/seed.js
+++ tasks/seed.js
@@ -8,17 +8,20 @@ const main = async () => {
     const db = await dbConnection();
     await db.dropDatabase();
 
-    let i;
-    for (i = 0; i < artistsData.length; i++) {
+    let artistsIds =[];
+    for (let i = 0; i < artistsData.length; i++) {
         const row = artistsData[i];
-        await artists.addArtist(row.name, row.genre, row.mtv);
-    }
-    await concerts.addConcert('star', ['5df51f871a9afd6f23d37791','5df51f871a9afd6f23d37792'], '2020-01-01', '20:00', '130 NY-22 Pawling, NY', '12564', 'Daryls House', ["pop", 'rock'], 'good', '100');
-
-    // await concerts.addConcert('star', ['5df517a3dc7bf36d8f9574d3','5df517a3dc7bf36d8f9574d4'], '2019-01-01', '20:00', '130 NY-22 Pawling, NY', '12564', 'Daryls House', ["pop", 'rock'], 'good', '100');
-    // await concerts.addConcert('moon', ['5df517a3dc7bf36d8f9574d5','5df517a3dc7bf36d8f9574d6'], '2020-01-01', '20:00', '24 fifth street, hoboken, NJ', '07030', 'DebaunPAC', ["Hip-Hop", 'rock'], 'excellent', '200');
-    // await concerts.addConcert('sun', ['5df517a3dc7bf36d8f9574d6','5df517a3dc7bf36d8f9574d3'], '2019-06-01', '20:00', '130 NY-22 Pawling, NY', '12564', 'Daryls House', ["pop", 'Country'], 'wonderful', '300');
-    // await concerts.addConcert('earth', ['5df517a3dc7bf36d8f9574d7','5df517a3dc7bf36d8f9574d5'], '2020-06-01', '20:00', '24 fifth street, hoboken, NJ', '07030', 'DebaunPAC', ["rap", 'house'], 'perfect', '400');
+        let newArtist = await artists.addArtist(row.name, row.genre, row.mtv);
+        artistsIds.push(String(newArtist._id));
+    };
+    const artists1 = [artistsIds.pop(),artistsIds.pop()];
+    const artists2 = [artistsIds.pop(),artistsIds.pop()];
+    const artists3 = [artistsIds.pop(),artistsIds.pop()];
+    const artists4 = [artistsIds.pop(),artistsIds.pop()];
+    await concerts.addConcert('star', artists1, '2019-01-01', '20:00', '130 NY-22 Pawling, NY', '12564', 'Daryls House', ["pop", 'rock'], 'good', '100');
+    await concerts.addConcert('moon', artists2, '2020-01-01', '20:00', '24 fifth street, hoboken, NJ', '07030', 'DebaunPAC', ["Hip-Hop", 'rock'], 'excellent', '200');
+    await concerts.addConcert('sun', artists3, '2019-06-01', '20:00', '130 NY-22 Pawling, NY', '12564', 'Daryls House', ["pop", 'Country'], 'wonderful', '300');
+    await concerts.addConcert('earth', artists4, '2020-06-01', '20:00', '24 fifth street, hoboken, NJ', '07030', 'DebaunPAC', ["rap", 'house'], 'perfect', '400');
 
 
 
