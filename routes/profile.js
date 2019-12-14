ca35cad87bfffb1c7e1dd1e2fda374d8d9444881
vpilly <varun.pilly@gmail.com>
2019-12-14 17:07:05 -0500

    updated viewbars

---
+++ routes/profile.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+
+module.exports = router;
